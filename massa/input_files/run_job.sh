ed2705bd501fef634492715b38580a0e7e342587
Sara Polselli <polselli@hpc-fe1.lamma.it>
2024-10-17 15:06:59 +0000

    Sbatch script to add jobs to the slurm queue

---
+++ massa/input_files/run_job.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+
+#SBATCH --ntasks 1
+##SBATCH --output /OCEANASTORE/progetti/funwave/output_massa/output_files/log/fw_ms_irr_2x2_pt.%j.out
+#SBATCH --output /OCEANASTORE/progetti/funwave/output_massa/output_files/log/%x.%j.out
+##SBATCH --error /OCEANASTORE/progetti/funwave/output_massa/output_files/log/fw_ms_irr_2x2_pt.%j.err
+#SBATCH --error /OCEANASTORE/progetti/funwave/output_massa/output_files/log/%x.%j.err
+#SBATCH --quiet
+
+# Usage: 
+#   sbatch --job-name [run] --ntasks 28 ./run_job.sh
+# or
+#   sbatch -J [run] -n 28 ./run_job.sh
+#       where [run] is the same suffix of the input file i.e. input_[run].txt
+
+echo "Job started"
+
+# >> Exit if run argument is missing
+# if [[ $SLURM_JOB_NAME == *.sh ]]; then 
+#     echo "Add option --job-name [run] (or -J) before script name: e.g. irr_2x2_pt"
+#     exit 1
+# else
+#     echo "Job name is $SLURM_JOB_NAME"
+# fi
+if [[ $SLURM_JOB_NAME == *.sh ]]; then
+    echo "Specify the run name: e.g. irr_2x2_pt"
+    exit 1
+else
+    echo "Job name is $SLURM_JOB_NAME"
+fi
+# <<
+# >> Exit if number of tasks argument is missing
+echo $SLURM_NTASKS
+if [[ -z "$SLURM_NTASKS"  || $SLURM_NTASKS -eq 1 ]]; then
+    echo "Specify the number of tasks: e.g. --ntasks 28"
+    exit 1
+else
+    echo "Number or tasks is $SLURM_NTASKS"
+fi
+# <<
+
+source /cluster/env/C7-INT2019U3-NC473-OMPI407-AVX512.env
+
+# Working folder
+DIR="${HOME}/FUNWAVE/FUNWAVE-TVD-IT-MASSA/massa/input_files"
+cd $DIR
+
+# Insert here the process to be lunched with mpirun
+echo "mpirun -np $SLURM_NTASKS ./funwave--intel-parallel-single input_massa_$SLURM_JOB_NAME.txt"
+# mpirun -np 28 ./funwave--intel-parallel-single input_massa_irr30deg.txt
+mpirun -np $SLURM_NTASKS ./funwave--intel-parallel-single input_massa_$SLURM_JOB_NAME.txt
+echo "Job finished"
--- massa/input_files/run_job_irr_2x2_pt.sh
+++ massa/input_files/run_job_irr_2x2_pt.sh
@@ -5,19 +5,23 @@
 # SBATCH instructions for SLURM: 
 #   the job name, the proc number, and where the output and err files will be saved
 
-#SBATCH --job-name=fw_ms_irr_30deg
+##SBATCH --job-name=fw_ms_irr_30deg
+#SBATCH --job-name=fw_ms_irr
 #SBATCH --ntasks=28
-#SBATCH --output /home/mare/funwave/output_massa/output_files/log/fw_ms_irr_30deg.%j.out
-#SBATCH --error /home/mare/funwave/output_massa/output_files/log/fw_ms_irr_30deg.%j.err
+##SBATCH --output /home/mare/funwave/output_massa/output_files/log/fw_ms_irr_30deg.%j.out
+#SBATCH --output /OCEANASTORE/progetti/funwave/output_massa/output_files/log/fw_ms_irr_2x2_pt.%j.out
+##SBATCH --error /home/mare/funwave/output_massa/output_files/log/fw_ms_irr_30deg.%j.err
+#SBATCH --error /OCEANASTORE/progetti/funwave/output_massa/output_files/log/fw_ms_irr_2x2_pt.%j.err
 #SBATCH --quiet
 
 # source /etc/profile
 source /cluster/env/C7-INT2019U3-NC473-OMPI407-AVX512.env
 
 # Working folder
-DIR="${HOME}/funwave/FUNWAVE-TVD/massa/input_files"
+DIR="${HOME}/funwave/FUNWAVE-TVD-IT-MASSA/massa/input_files"
 cd $DIR
 
 # Inset here the process to be lunched with mpirun
 # exec with mpi -
-mpirun -np 28 ./funwave--intel-parallel-single input_massa_irr30deg.txt
+# mpirun -np 28 ./funwave--intel-parallel-single input_massa_irr30deg.txt
+mpirun -np 28 ./funwave--intel-parallel-single input_massa_irr_2x2_pt.txt
